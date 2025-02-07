ba06ddd7be5c26854ed7f53021a847bbc81a9df7
DreamMakerW <abcwyy123456@163.com>
2025-02-07 22:10:24 +0800

    4-9 Apply Gameplay Effect To Self

--- Source/Warrior/Private/DataAssets/StartUpData/DataAsset_StartUpDataBase.cpp
+++ Source/Warrior/Private/DataAssets/StartUpData/DataAsset_StartUpDataBase.cpp
@@ -15,6 +15,21 @@ void UDataAsset_StartUpDataBase::GiveToAbilitySystemComponent(UWarriorAbilitySys
 	
 	GrantAbilities(ActivateOnGivenAbilities, InASCToGive, ApplyLevel);
 	GrantAbilities(ReactiveAbilities, InASCToGive, ApplyLevel);
+
+	if (!StartUpGameplayEffects.IsEmpty())
+	{
+		for (const TSubclassOf<UGameplayEffect>& EffectClass : StartUpGameplayEffects)
+		{
+			if (!EffectClass) continue;
+			// CDO is short for Class Default Object
+			UGameplayEffect* EffectCDO = EffectClass->GetDefaultObject<UGameplayEffect>();
+			InASCToGive->ApplyGameplayEffectToSelf(
+				EffectCDO,
+				ApplyLevel,
+				InASCToGive->MakeEffectContext()
+			);
+		}
+	}
 }
 
 void UDataAsset_StartUpDataBase::GrantAbilities(const TArray<TSubclassOf<UWarriorGameplayAbility>>& InAbilitiesToGive, UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel)
