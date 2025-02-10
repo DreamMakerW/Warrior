df17e0e0703bbbc8332d6555ea4f9a0e67f3c0c0
DreamMakerW <abcwyy123456@163.com>
2025-02-10 23:50:35 +0800

    4-20 Capture Relevant Attributes

--- Source/Warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
+++ Source/Warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
@@ -2,4 +2,47 @@
 
 
 #include "AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h"
+#include "AbilitySystem/WarriorAttributeSet.h"
 
+struct FWarriorDamageCapture
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower)
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower)
+
+	FWarriorDamageCapture()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, AttackPower, Source, false)
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DefensePower, Target, false)
+	}
+};
+
+static const FWarriorDamageCapture& GetWarriorDamageCapture()
+{
+	static FWarriorDamageCapture WarriorDamageCapture;
+	return WarriorDamageCapture;
+}
+
+UGEExecCalc_DamageTaken::UGEExecCalc_DamageTaken()
+{
+	// 获取所有需要的attributes
+
+	/* Slow way of doing capture */
+	/*FProperty* AttackPowerProperty = FindFieldChecked<FProperty>(
+		UWarriorAttributeSet::StaticClass(),
+		GET_MEMBER_NAME_CHECKED(UWarriorAttributeSet, AttackPower)
+	);
+
+	// 以上与DECLARE_ATTRIBUTE_CAPTUREEF(AttackPower)等效
+
+	FGameplayEffectAttributeCaptureDefinition AttackPowerDefinition(
+		AttackPowerProperty,
+		EGameplayEffectAttributeCaptureSource::Source,
+		false
+	);
+
+	RelevantAttributesToCapture.Add(AttackPowerDefinition);*/
+
+
+	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().AttackPowerDef);
+	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DefensePowerDef);
+}
