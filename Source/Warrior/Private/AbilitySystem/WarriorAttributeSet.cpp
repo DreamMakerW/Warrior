52d6c275cf3a2771ac761bb469bff3eb1d91106a
DreamMakerW <abcwyy123456@163.com>
2025-02-16 03:44:27 +0800

    4-32 Enemy Death Ability

--- Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/WarriorAttributeSet.h"
 #include "GameplayEffectExtension.h"
+#include "WarriorFunctionLibrary.h"
+#include "WarriorGameplayTags.h"
 
 #include "WarriorDebugHelper.h"
 
@@ -60,10 +62,9 @@ void UWarriorAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCal
 
 		// TODO: Notify the UI
 
-		// TODO: Handle character death
 		if (NewCurrentHealth == 0.f)
 		{
-
+			UWarriorFunctionLibrary::AddGameplayTagToActorIfNone(Data.Target.GetAvatarActor(), WarriorGameplayTags::Shared_Status_Dead);
 		}
 	}
 }
