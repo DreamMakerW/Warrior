5982fd0fa5bd8863ad1715813a746832e61ae47c
DreamMakerW <abcwyy123456@163.com>
2025-02-17 23:28:58 +0800

    4-38 Listen For Broadcasting

---
+++ Source/Warrior/Public/Widgets/WarriorWidgetBase.h
@@ -0,0 +1,25 @@
+// Wyy All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WarriorWidgetBase.generated.h"
+
+class UHeroUIComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeOnInitialized() override;
+
+	UFUNCTION(BlueprintImplementableEvent, meta = (Display = "On Owning Hero UI Component Initialized"))
+	void BP_OnOwningHeroUIComponentInitialized(UHeroUIComponent* OwningHeroUIComponent);
+	
+};
