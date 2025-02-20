3ab0eb07f421401e4ce87dfd1b287c78f3a2a9ca
DreamMakerW <abcwyy123456@163.com>
2025-02-21 00:19:25 +0800

    5-4 AI Perception

--- Source/Warrior/Private/Controllers/WarriorAIController.cpp
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -3,6 +3,8 @@
 
 #include "Controllers/WarriorAIController.h"
 #include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
 #include "WarriorDebugHelper.h"
 
@@ -14,4 +16,24 @@ AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectIniti
 		Debug::Print(TEXT("CrowdFollowingComponent valid"), FColor::Green);
 	}
 
+	AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("EnemySenseConfig_Sight");
+	// 配置内部属性
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	// 只能看见敌对pawn
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	AISenseConfig_Sight->SightRadius = 5000.f;
+	// 敌人永远不会丢失玩家的视野
+	AISenseConfig_Sight->LoseSightRadius = 0.f;
+	AISenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;
+
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("EnemyPerceptionComponent");
+	EnemyPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &ThisClass::OnEnemyPerceptionUpdated);
+
+}
+
+void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
 }
