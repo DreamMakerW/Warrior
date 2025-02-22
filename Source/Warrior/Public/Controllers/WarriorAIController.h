d1bcb72884e2492fb772702a10aefcce6be47f95
DreamMakerW <abcwyy123456@163.com>
2025-02-22 18:01:20 +0800

    5-7 Configure AI Avoidance

--- Source/Warrior/Public/Controllers/WarriorAIController.h
+++ Source/Warrior/Public/Controllers/WarriorAIController.h
@@ -25,6 +25,8 @@ public:
 	//~ End IGenericTeamAgentInterface Interface
 
 protected:
+	virtual void BeginPlay() override;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	UAIPerceptionComponent* EnemyPerceptionComponent;
 
@@ -33,4 +35,18 @@ protected:
 
 	UFUNCTION()
 	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Detour Crowd Avoidance Config")
+	bool bEnableDetourCrowdAvoidance = true;
+
+	// 当bEnableDetourCrowdAvoidance为true时，该变量才可以进行调整
+	UPROPERTY(EditDefaultsOnly, Category = "Detour Crowd Avoidance Config", meta = (EditCondition = "bEnableDetourCrowdAvoidance", UIMin = "1", UIMax = "4"))
+	int32 DetourCrowdAvoidanceQuality = 4;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Detour Crowd Avoidance Config", meta = (EditCondition = "bEnableDetourCrowdAvoidance"))
+	float CollisionQueryRange = 600.f;
+
+
+
 };
