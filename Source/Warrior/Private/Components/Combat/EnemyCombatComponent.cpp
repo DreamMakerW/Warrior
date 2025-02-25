40a1365f630490548969cb09147053b2e97d185a
DreamMakerW <abcwyy123456@163.com>
2025-02-25 23:14:37 +0800

    5-21 Notify Melee Hit

--- Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -2,12 +2,48 @@
 
 
 #include "Components/Combat/EnemyCombatComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "WarriorGameplayTags.h"
+
 #include "WarriorDebugHelper.h"
+
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	// 防止同一actor的重复判定
+	if (OverlappedActors.Contains(HitActor))
 	{
-		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+		return;
+	}
+
+	OverlappedActors.Add(HitActor);
+
+	// TODO:: Implement block check
+	bool bIsValidBlock = false;
+
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnblockable = false;
+
+	if (bIsPlayerBlocking && !bIsMyAttackUnblockable)
+	{
+		// TODO:: check if the block is valid
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;
+
+
+	if (bIsValidBlock)
+	{
+		// TODO:: Handle successful block
+	}
+	else
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			WarriorGameplayTags::Shared_Event_MeleeHit,
+			EventData
+		);
 	}
 }
