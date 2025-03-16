b4b67e6294dcca592050f819f966687a95728d82
DreamMakerW <abcwyy123456@163.com>
2025-03-16 22:20:24 +0800

    7-12 Projectile Spec Handle

--- Source/Warrior/Private/Items/WarriorProjectileBase.cpp
+++ Source/Warrior/Private/Items/WarriorProjectileBase.cpp
@@ -89,6 +89,7 @@ void AWarriorProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent, 
 	else
 	{
 		// Apply projectile damage
+		HandleApplyProjectileDamage(HitPawn, Data);
 	}
 
 	Destroy();
@@ -96,6 +97,23 @@ void AWarriorProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent, 
 
 void AWarriorProjectileBase::OnProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+
+}
+
+void AWarriorProjectileBase::HandleApplyProjectileDamage(APawn* InHitPawn, const FGameplayEventData& InPayload)
+{
+	checkf(ProjectileDamageEffectSpecHandle.IsValid(), TEXT("Forget to assgin a valid spec handle to the projectile: %s"), *GetActorNameOrLabel());
+	
+	const bool bWasApplied = UWarriorFunctionLibrary::ApplyGameplayEffectSpecHandleToTargetActor(GetInstigator(), InHitPawn, ProjectileDamageEffectSpecHandle);
+	
+	if (bWasApplied)
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			InHitPawn,
+			WarriorGameplayTags::Shared_Event_HitReact,
+			InPayload
+		);
+	}
 }
 
 
