c1ebb33613c7a94f9f4c1c816f5f077f3d2976fa
DreamMakerW <abcwyy123456@163.com>
2025-04-19 16:22:43 +0800

    11-24 Loading Screen

--- Source/Warrior/Private/WarriorGameInstance.cpp
+++ Source/Warrior/Private/WarriorGameInstance.cpp
@@ -2,6 +2,36 @@
 
 
 #include "WarriorGameInstance.h"
+#include "MoviePlayer.h"
+#include "WarriorDebugHelper.h"
+
+void UWarriorGameInstance::Init()
+{
+    Super::Init();
+
+    FCoreUObjectDelegates::PreLoadMap.AddUObject(this, &ThisClass::OnPreLoadMap);
+    FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &ThisClass::OnDestinationWorldLoaded);
+
+}
+
+
+void UWarriorGameInstance::OnPreLoadMap(const FString& MapName)
+{
+    FLoadingScreenAttributes LoadingScreenAttributes;
+    LoadingScreenAttributes.bAutoCompleteWhenLoadingCompletes = true;
+    // 加载屏幕至少加载时间
+    LoadingScreenAttributes.MinimumLoadingScreenDisplayTime = 2.f;
+    LoadingScreenAttributes.WidgetLoadingScreen = FLoadingScreenAttributes::NewTestLoadingScreenWidget();
+
+    // 通过该方式加载loading screen，确保loading screen运行在不同的线程上
+    GetMoviePlayer()->SetupLoadingScreen(LoadingScreenAttributes);
+}
+
+void UWarriorGameInstance::OnDestinationWorldLoaded(UWorld* LoadedWorld)
+{
+    // close the loading screen
+    GetMoviePlayer()->StopMovie();
+}
 
 TSoftObjectPtr<UWorld> UWarriorGameInstance::GetGameLevelByTag(FGameplayTag InTag) const
 {
